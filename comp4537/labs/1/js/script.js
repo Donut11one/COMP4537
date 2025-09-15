f0fc8845001bd7b751e63d749c2bc669eb57729d
Alex He <donut11one@gmail.com>
2025-09-15 00:11:58 -0700

    more console logs

--- comp4537/labs/1/js/script.js
+++ comp4537/labs/1/js/script.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Determine the current page
     const isWriterPage = window.location.pathname.includes('writer.html');
+    console.log(isWritePage)
+
     const notesContainer = document.getElementById('notes-container');
 
     // Instantiate the NoteManager to run the application logic
