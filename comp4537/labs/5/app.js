a97925bd4bc6130f4c742c5de3dab0364c61b5e3
Alex He <donut11one@gmail.com>
2025-10-17 13:11:07 -0700

    car3

--- comp4537/labs/5/app.js
+++ comp4537/labs/5/app.js
@@ -112,9 +112,9 @@ const server = http.createServer((req, res) => {
             const entryExists = dictionary.some(item => item.word.toLowerCase() === normalizedWord);
 
             if(entryExists === "car")
-                return sendJsonResponse("VROOM VROOM");
+                return "VROOM VROOM";
 
-            if (entryExists) {
+            else if (entryExists) {
                 // Conflict: 409 Conflict (Word already exists)
                 return sendJsonResponse(res, 409, MESSAGES.WARNING_EXISTS(requestCount, word.trim()));
             } else {
